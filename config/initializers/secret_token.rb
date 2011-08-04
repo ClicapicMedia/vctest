cf1050a6a6009d151c77286e4bf5d01d2600ddc3
sean <sean@clicapic.com>
2011-08-04 17:01:25 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '6256e94fb735e9d3d490c572611c6f4e6ba0d15148d270e4921ab06576a903b92835a939bcedebdcb04470ad1e07ddff3c69fb265688a2b13f4e1d2edbf51436'
